de0c50914253be15b0df9564860df7c35b3371ac
Mark <Markie129111@gmail.com>
2023-05-05 20:33:42 -0400

    Initial commit

---
+++ src/main/java/com/elementalfishing/ElementalFishing.java
@@ -0,0 +1,17 @@
+package com.elementalfishing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ElementalFishing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
